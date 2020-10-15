1e9a4b5092b06c49e2394f1d4db27bbf2e4c34e1
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-10-15 19:24:37 -0400

    added contact section

--- my-app/src/components/Contacts.js
+++ my-app/src/components/Contacts.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
 import '../styles/Contacts.css'
+import github_logo from '../images/github_logo.svg'
+import linkedin_logo from '../images/linkedin_logo.svg'
+
 
 class Contacts extends Component{
     render(){
@@ -8,10 +12,18 @@ class Contacts extends Component{
                 <div className='title-contacts'>
                     <h1>Contacts</h1>
                 </div>
+                <div className='email-section'>jiachengli1999@gmail.com</div>
                 <div className='contactBoxes-container'>
-                    <div>Box1</div>
-                    <div>Box2</div>
-                    <div>Box3</div>
+                    <div>
+                        <a href='https://github.com/jiachengli1999?tab=repositories'>
+                            <img src={github_logo}/>
+                        </a>
+                    </div>
+                    <div>
+                        <a href='https://www.linkedin.com/in/jia-cheng-li'>
+                            <img src={linkedin_logo}/>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
