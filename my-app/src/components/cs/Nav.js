9ddcaaac1eb6a59430da2e8987774d00e1f30be4
jiachengli1999 <35944405+jiachengli1999@users.noreply.github.com>
2020-10-16 10:24:30 -0400

    added active class while scrolling

--- my-app/src/components/cs/Nav.js
+++ my-app/src/components/cs/Nav.js
@@ -10,10 +10,18 @@ class Nav extends Component{
         return(
             <div className='container-flex-nav'>
                 <div className='nav-item1'>
-                    <Link to="about-page" spy={true} smooth={true} offset={-30} duration={500}>About Me</Link>
-                    <Link to="projects-page" spy={true} smooth={true} offset={20} duration={500}>Projects</Link>
-                    <Link to="works-page" spy={true} smooth={true} offset={-30} duration={500}>Work Experience</Link>
-                    <Link to="contacts-page" spy={true} smooth={true} offset={50} duration={500}>Contacts</Link>
+                    <Link to="about-page" activeClass='active' spy={true} smooth={true} offset={-30} duration={500}>
+                        About Me
+                    </Link>
+                    <Link to="projects-page" activeClass='active' spy={true} smooth={true} offset={20} duration={500}>
+                        Projects
+                    </Link>
+                    <Link to="works-page" activeClass='active' spy={true} smooth={true} offset={20} duration={500}>
+                        Work Experience
+                    </Link>
+                    <Link to="contacts-page" activeClass='active' spy={true} smooth={true} offset={50} duration={500}>
+                        Contacts
+                    </Link>
                 </div>
                 <div className='nav-item2'>
                     <label>3D Modeling</label>
